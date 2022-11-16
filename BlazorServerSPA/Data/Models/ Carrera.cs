5d5a975011b3033c4b9779c4cdd5593c4d943465
Yancony <117234595+Yancony@users.noreply.github.com>
2022-11-16 17:11:22 +0100

    Intefaz (nuevamente)

---
+++ BlazorServerSPA/Data/Models/ Carrera.cs
@@ -0,0 +1,11 @@
+namespace ESTUDIANTES20171645.Data.Models;
+
+public class Carrera
+{
+    public Carrera()
+    {
+        
+    }
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+}
